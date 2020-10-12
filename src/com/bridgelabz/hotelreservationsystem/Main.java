c27c653b51cf78cb8aa9137b76a0f0400fc96010
unknown <divyacharaninabha@gmail.com>
2020-10-13 00:10:07 +0530

    Added: Welcome message added

---
+++ src/com/bridgelabz/hotelreservationsystem/Main.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.hotelreservationsystem;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Welcome Message
+		System.out.println("Welcome to Hotel Reservation System");
+
+	}
+
+}
