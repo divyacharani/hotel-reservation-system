7b6aefad4720ed574ff28a4b145f5760c47ddc8a
unknown <divyacharaninabha@gmail.com>
2020-10-14 11:45:37 +0530

    Added : Functionality to validate input using regex

--- src/com/bridgelabz/hotelreservationsystem/HotelReservation.java
+++ src/com/bridgelabz/hotelreservationsystem/HotelReservation.java
@@ -2,6 +2,7 @@ package com.bridgelabz.hotelreservationsystem;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
@@ -42,51 +43,36 @@ public class HotelReservation {
 	}
 
 	// To calculate total rate
-	public long calculateTotalRate(Hotel hotel, int type) {
+	public long calculateTotalRate(Hotel hotel, String type) {
 		long hotelRate = 0;
-		if (type == 1)
+		if (type.equals("regular") || type.equals("Regular"))
 			hotelRate = hotel.getRateForRegularCustomerWeekDay() * noOfWeekDays
 					+ hotel.getRateForRegularCustomerWeekEnd() * noOfWeekEnds;
-		else if (type == 2)
+		else if (type.equals("reward") || type.equals("Reward"))
 			hotelRate = hotel.getRateForRewardCustomerWeekDay() * noOfWeekDays
 					+ hotel.getRateForRewardCustomerWeekEnd() * noOfWeekEnds;
 		return hotelRate;
 	}
 
+	// To set total rates for hotels
+	public void setTotalRatesForHotels(String type) {
+		hotelList.stream().forEach(hotel -> hotel.setTotalRate(calculateTotalRate(hotel, type)));
+	}
+
 	// To find cheapest best rated hotel
-	public Hotel cheapestBestHotel(int type) {
-		Hotel cheapHotel = new Hotel();
-		long totalRate = Long.MAX_VALUE;
-		String hotelName = "";
-		for (Hotel hotel : hotelList) {
-			long hotelRate = calculateTotalRate(hotel, type);
-			if (hotelRate < totalRate || (hotelRate == totalRate && hotel.getRating() > cheapHotel.getRating())) {
-				totalRate = hotelRate;
-				hotelName = hotel.getHotelName();
-				cheapHotel.setRating(hotel.getRating());
-			}
-		}
-		cheapHotel.setHotelName(hotelName);
-		cheapHotel.setTotalRate(totalRate);
+	public Hotel cheapestBestHotel(String type) {
+		setTotalRatesForHotels(type);
+		long minimumRate = hotelList.stream().mapToLong(Hotel::getTotalRate).min().orElse(0);
+		Hotel cheapHotel = hotelList.stream().filter(hotel -> (hotel.getTotalRate() == minimumRate))
+				.max(Comparator.comparingInt(Hotel::getRating)).orElse(null);
 		return cheapHotel;
 	}
 
 	// To find best rated hotel
-	public Hotel bestRatedHotel(int type) {
-		Hotel bestHotel = new Hotel();
-		long totalRate = Long.MAX_VALUE;
-		String hotelName = "";
-		for (Hotel hotel : hotelList) {
-			long hotelRate = calculateTotalRate(hotel, type);
-			if (hotel.getRating() > bestHotel.getRating()) {
-				totalRate = hotelRate;
-				hotelName = hotel.getHotelName();
-				bestHotel.setRating(hotel.getRating());
-			}
-		}
-		bestHotel.setHotelName(hotelName);
-		bestHotel.setTotalRate(totalRate);
-		return bestHotel;
+	public Hotel bestRatedHotel(String type) {
+		setTotalRatesForHotels(type);
+		Hotel besthotel = hotelList.stream().max(Comparator.comparingInt(Hotel::getRating)).orElse(null);
+		return besthotel;
 	}
 
 	public static void main(String[] args) {
@@ -102,8 +88,18 @@ public class HotelReservation {
 		hotelReservation.addHotel("Lakewood", 110, 90, 3, 80, 80);
 		hotelReservation.addHotel("Bridgewood", 150, 50, 4, 110, 50);
 		hotelReservation.addHotel("Ridgewood", 220, 150, 5, 100, 40);
-		System.out.println("Enter customer type \n1. Regular\n2. Reward");
-		int type = sc.nextInt();
+		boolean isValidate = false;
+		String type = "";
+		while (true) {
+			System.out.println("Enter customer type Regular/Reward");
+			type = sc.nextLine();
+			isValidate = type.matches("^regular|Regular|reward|Reward$");
+			if (isValidate)
+				break;
+			else
+				System.out.println("Invalid!!");
+		}
+
 		System.out.println("Enter the start date in ddMMMYYYY format");
 		String startDate = sc.next();
 		System.out.println("Enter the end date in ddMMMYYYY format");
